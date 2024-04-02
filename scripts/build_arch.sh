c1c0b78044f2c2fcb1b6eb5fe0a832bf70235fbc
Anders Pitman <&ers@apitman.com>
2024-04-02 12:33:32 -0600

    Update build scripts

--- scripts/build_arch.sh
+++ scripts/build_arch.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
 
-echo Building platform $1-$2
-CGO_ENABLED=0 GOOS=$1 GOARCH=$2 go build -ldflags "-X main.Version=$(git describe --tags)" -o build/obligator-$1-$2$3
+os=$1
+arch_name=$2
+
+version=$(git describe --tags)
+arch=$arch_name
+file_extension=
+
+if [[ "$os" == "windows" ]]
+then
+    export CC=x86_64-w64-mingw32-gcc
+    file_extension=".exe"
+fi
+
+if [[ "$arch_name" == "arm64" ]]
+then
+    export CC=aarch64-linux-gnu-gcc
+fi
+
+if [[ "$arch_name" == "x64" ]]
+then
+    arch="amd64"
+fi
+
+filename=obligator-$os-${arch_name}-$version$file_extension
+
+echo Building platform $os-$arch_name
+CGO_ENABLED=1 GOOS=$os GOARCH=$arch go build \
+    -C ./cmd/obligator \
+    -ldflags "-extldflags=-static -X main.Version=$version" \
+    -tags sqlite_omit_load_extension,netgo \
+    -o ../../build/$filename
+
+if [[ "$os" != "windows" ]]
+then
+    tar czf build/$filename.tar.gz -C build $filename
+    rm build/$filename
+fi
--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -2,22 +2,18 @@
 
 version=$(git describe --tags)
 
-./scripts/build_x86_64.sh linux
-./scripts/build_x86_64.sh freebsd
-./scripts/build_x86_64.sh openbsd
-./scripts/build_x86_64.sh windows .exe
-./scripts/build_x86_64.sh darwin
-
-./scripts/build_arch.sh linux 386
-./scripts/build_arch.sh linux arm
+./scripts/build_arch.sh linux x64
+#./scripts/build_arch.sh linux 386
+#./scripts/build_arch.sh linux arm
 ./scripts/build_arch.sh linux arm64
-./scripts/build_arch.sh freebsd 386
-./scripts/build_arch.sh freebsd arm
-./scripts/build_arch.sh freebsd arm64
-./scripts/build_arch.sh openbsd 386
-./scripts/build_arch.sh openbsd arm
-./scripts/build_arch.sh openbsd arm64
-./scripts/build_arch.sh windows 386 .exe
+#./scripts/build_arch.sh freebsd 386
+#./scripts/build_arch.sh freebsd arm
+#./scripts/build_arch.sh freebsd arm64
+#./scripts/build_arch.sh openbsd 386
+#./scripts/build_arch.sh openbsd arm
+#./scripts/build_arch.sh openbsd arm64
+#./scripts/build_arch.sh windows 386 .exe
+./scripts/build_arch.sh windows x64
 
 
 tar -czf ./obligator_${version}.tar.gz build/
