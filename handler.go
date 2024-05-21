40c7aef7269a7006a468725fb7180bcdf7d8bb54
Anders Pitman <&ers@apitman.com>
2024-05-21 11:43:29 -0600

    Move endpoints into separate file

---
+++ handler.go
@@ -0,0 +1,171 @@
+package obligator
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
+)
+
+type Handler struct {
+	mux *http.ServeMux
+}
+
+func NewHandler(storage Storage, conf ServerConfig, tmpl *template.Template) *Handler {
+
+	mux := http.NewServeMux()
+
+	h := &Handler{
+		mux: mux,
+	}
+
+	prefix := storage.GetPrefix()
+	loginKeyName := prefix + "login_key"
+
+	publicJwks, err := jwk.PublicSetOf(storage.GetJWKSet())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	mux.HandleFunc("/ip", func(w http.ResponseWriter, r *http.Request) {
+		remoteIp, err := getRemoteIp(r, conf.BehindProxy)
+		if err != nil {
+			w.WriteHeader(500)
+			io.WriteString(w, err.Error())
+			return
+		}
+
+		data := struct {
+			RootUri     string
+			DisplayName string
+			RemoteIp    string
+		}{
+			RootUri:     storage.GetRootUri(),
+			DisplayName: storage.GetDisplayName(),
+			RemoteIp:    remoteIp,
+		}
+
+		err = tmpl.ExecuteTemplate(w, "ip.html", data)
+		if err != nil {
+			w.WriteHeader(500)
+			io.WriteString(w, err.Error())
+			return
+		}
+	})
+
+	mux.HandleFunc("/validate", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+
+		redirectUri := r.Form.Get("redirect_uri")
+		url := fmt.Sprintf("%s/auth?client_id=%s&redirect_uri=%s&response_type=code&state=&scope=",
+			storage.GetRootUri(), redirectUri, redirectUri)
+
+		loginKeyCookie, err := r.Cookie(loginKeyName)
+		if err != nil {
+			http.Redirect(w, r, url, 307)
+			return
+		}
+
+		// TODO: add Remote-Email to header
+		_, err = jwt.Parse([]byte(loginKeyCookie.Value), jwt.WithKeySet(publicJwks))
+		if err != nil {
+			http.Redirect(w, r, url, 307)
+			return
+		}
+	})
+
+	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+
+		idents, _ := getIdentities(storage, r, publicJwks)
+
+		canEmail := true
+		if _, err := storage.GetSmtpConfig(); err != nil {
+			canEmail = false
+		}
+
+		providers, err := storage.GetOAuth2Providers()
+		if err != nil {
+			w.WriteHeader(500)
+			io.WriteString(w, err.Error())
+			return
+		}
+
+		data := struct {
+			DisplayName     string
+			CanEmail        bool
+			Identities      []*Identity
+			OAuth2Providers []OAuth2Provider
+			LogoMap         map[string]template.HTML
+			ReturnUri       string
+		}{
+			DisplayName:     storage.GetDisplayName(),
+			CanEmail:        canEmail,
+			Identities:      idents,
+			OAuth2Providers: providers,
+			LogoMap:         providerLogoMap,
+			ReturnUri:       "/login",
+		}
+
+		err = tmpl.ExecuteTemplate(w, "login.html", data)
+		if err != nil {
+			w.WriteHeader(500)
+			io.WriteString(w, err.Error())
+			return
+		}
+	})
+
+	mux.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method != http.MethodPost {
+			w.WriteHeader(405)
+			return
+		}
+
+		r.ParseForm()
+
+		redirect := r.Form.Get("prev_page")
+
+		err = deleteLoginKeyCookie(storage, w)
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(os.Stderr, err.Error())
+		}
+
+		w.Header().Add("Set-Login", "logged-out")
+		http.Redirect(w, r, redirect, http.StatusSeeOther)
+	})
+
+	mux.HandleFunc("/no-account", func(w http.ResponseWriter, r *http.Request) {
+		data := struct {
+			URL         string
+			RootUri     string
+			DisplayName string
+		}{
+			URL:         fmt.Sprintf("/auth?%s", r.URL.RawQuery),
+			RootUri:     storage.GetRootUri(),
+			DisplayName: storage.GetDisplayName(),
+		}
+
+		err = tmpl.ExecuteTemplate(w, "no-account.html", data)
+		if err != nil {
+			w.WriteHeader(500)
+			io.WriteString(w, err.Error())
+			return
+		}
+	})
+
+	mux.HandleFunc("/debug", func(w http.ResponseWriter, r *http.Request) {
+		printJson(r.Header)
+	})
+
+	return h
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.mux.ServeHTTP(w, r)
+}
--- obligator.go
+++ obligator.go
@@ -147,7 +147,6 @@ func NewServer(conf ServerConfig) *Server {
 	}
 
 	prefix := storage.GetPrefix()
-	loginKeyName := prefix + "login_key"
 
 	//sqliteStorage, err := NewSqliteStorage(prefix + "storage.sqlite")
 	//if err != nil {
@@ -207,12 +206,6 @@ func NewServer(conf ServerConfig) *Server {
 		storage.AddJWKKey(key)
 	}
 
-	publicJwks, err := jwk.PublicSetOf(storage.GetJWKSet())
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
-	}
-
 	tmpl, err := template.ParseFS(fs, "templates/*")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
@@ -230,8 +223,17 @@ func NewServer(conf ServerConfig) *Server {
 		}
 	}
 
+	handler := NewHandler(storage, conf, tmpl)
+	mux.Handle("/", handler)
+
 	oidcHandler := NewOIDCHandler(storage, tmpl)
-	mux.Handle("/", oidcHandler)
+	mux.Handle("/.well-known/openid-configuration", oidcHandler)
+	mux.Handle("/jwks", oidcHandler)
+	mux.Handle("/register", oidcHandler)
+	mux.Handle("/userinfo", oidcHandler)
+	mux.Handle("/auth", oidcHandler)
+	mux.Handle("/approve", oidcHandler)
+	mux.Handle("/token", oidcHandler)
 
 	addIdentityOauth2Handler := NewAddIdentityOauth2Handler(storage, oauth2MetaMan)
 	mux.Handle("/login-oauth2", addIdentityOauth2Handler)
@@ -263,137 +265,6 @@ func NewServer(conf ServerConfig) *Server {
 	addIdentityFedCmHandler := NewAddIdentityFedCmHandler(storage, tmpl)
 	mux.Handle("/login-fedcm", addIdentityFedCmHandler)
 	mux.Handle("/complete-login-fedcm", addIdentityFedCmHandler)
-
-	mux.HandleFunc("/ip", func(w http.ResponseWriter, r *http.Request) {
-		remoteIp, err := getRemoteIp(r, conf.BehindProxy)
-		if err != nil {
-			w.WriteHeader(500)
-			io.WriteString(w, err.Error())
-			return
-		}
-
-		data := struct {
-			RootUri     string
-			DisplayName string
-			RemoteIp    string
-		}{
-			RootUri:     storage.GetRootUri(),
-			DisplayName: storage.GetDisplayName(),
-			RemoteIp:    remoteIp,
-		}
-
-		err = tmpl.ExecuteTemplate(w, "ip.html", data)
-		if err != nil {
-			w.WriteHeader(500)
-			io.WriteString(w, err.Error())
-			return
-		}
-	})
-
-	mux.HandleFunc("/validate", func(w http.ResponseWriter, r *http.Request) {
-		r.ParseForm()
-
-		redirectUri := r.Form.Get("redirect_uri")
-		url := fmt.Sprintf("%s/auth?client_id=%s&redirect_uri=%s&response_type=code&state=&scope=",
-			storage.GetRootUri(), redirectUri, redirectUri)
-
-		loginKeyCookie, err := r.Cookie(loginKeyName)
-		if err != nil {
-			http.Redirect(w, r, url, 307)
-			return
-		}
-
-		// TODO: add Remote-Email to header
-		_, err = jwt.Parse([]byte(loginKeyCookie.Value), jwt.WithKeySet(publicJwks))
-		if err != nil {
-			http.Redirect(w, r, url, 307)
-			return
-		}
-	})
-
-	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-
-		idents, _ := getIdentities(storage, r, publicJwks)
-
-		canEmail := true
-		if _, err := storage.GetSmtpConfig(); err != nil {
-			canEmail = false
-		}
-
-		providers, err := storage.GetOAuth2Providers()
-		if err != nil {
-			w.WriteHeader(500)
-			io.WriteString(w, err.Error())
-			return
-		}
-
-		data := struct {
-			DisplayName     string
-			CanEmail        bool
-			Identities      []*Identity
-			OAuth2Providers []OAuth2Provider
-			LogoMap         map[string]template.HTML
-			ReturnUri       string
-		}{
-			DisplayName:     storage.GetDisplayName(),
-			CanEmail:        canEmail,
-			Identities:      idents,
-			OAuth2Providers: providers,
-			LogoMap:         providerLogoMap,
-			ReturnUri:       "/login",
-		}
-
-		err = tmpl.ExecuteTemplate(w, "login.html", data)
-		if err != nil {
-			w.WriteHeader(500)
-			io.WriteString(w, err.Error())
-			return
-		}
-	})
-
-	mux.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
-
-		if r.Method != http.MethodPost {
-			w.WriteHeader(405)
-			return
-		}
-
-		r.ParseForm()
-
-		redirect := r.Form.Get("prev_page")
-
-		err = deleteLoginKeyCookie(storage, w)
-		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprintf(os.Stderr, err.Error())
-		}
-
-		w.Header().Add("Set-Login", "logged-out")
-		http.Redirect(w, r, redirect, http.StatusSeeOther)
-	})
-
-	mux.HandleFunc("/no-account", func(w http.ResponseWriter, r *http.Request) {
-		data := struct {
-			URL         string
-			RootUri     string
-			DisplayName string
-		}{
-			URL:         fmt.Sprintf("/auth?%s", r.URL.RawQuery),
-			RootUri:     storage.GetRootUri(),
-			DisplayName: storage.GetDisplayName(),
-		}
-
-		err = tmpl.ExecuteTemplate(w, "no-account.html", data)
-		if err != nil {
-			w.WriteHeader(500)
-			io.WriteString(w, err.Error())
-			return
-		}
-	})
-
-	mux.HandleFunc("/debug", func(w http.ResponseWriter, r *http.Request) {
-		printJson(r.Header)
-	})
 
 	s := &Server{
 		Config:  conf,
